bf000b824ffc8b8a557bb85eac7e61b8390f4a11
ChipoDev <ivan.grigorev.04@mail.ru>
2025-04-01 09:54:02 +0300

    first commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ReactiveUI;
+using ShapesApp.Models;
+using System.Collections.ObjectModel;
+
+namespace ShapesApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Shape> Shapes { get; } = new ObservableCollection<Shape>();
+
+        public void AddShape(Shape shape)
+        {
+            Shapes.Add(shape);
+        }
+
+        public void ClearShapes()
+        {
+            Shapes.Clear();
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ShapesApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
